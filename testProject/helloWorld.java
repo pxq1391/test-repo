1369cbd6ad8bb51aeed6f3df67b817cc2bc9a301
pxq1391 <pxq1391@g.rit.edu>
2018-02-20 18:06:48 -0500

    This is the commit message

--- testProject/helloWorld.java
+++ testProject/helloWorld.java
@@ -3,5 +3,7 @@ package test;
 public class helloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello world!");
+		System.out.println("We are in harmony.");
+		System.out.println("Death, is whimsical today.");
 	}
 }
